803e7839aff308795c42e9147f3b02feb2b69ee3
ichverstehe <ichverstehe@gmail.com>
2009-01-28 22:15:46 +0100

    bot!

---
+++ bot.rb
@@ -0,0 +1,17 @@
+require 'rubygems'
+require 'isaac'
+require 'rest_client'
+require 'json'
+
+config do |c|
+  c.nick    = 'nancie'
+  c.server  = 'irc.freenode.net'
+end
+
+on :connect do
+  join "#awesome_channel"
+end
+
+on :channel, /^nancie.*tweet this: (.*)/ do
+  RestClient.post 'http://sinatrarb:somepassword@twitter.com/statuses/update.json', :status => match[1]
+end
